c559107e071ad6390561fefbe5e86e498f69f884
Sina <Sina.baharlou@gmail.com>
2019-05-22 13:04:28 +0430

    Enhanced coding style and comments.

--- loadsetEx.m
+++ loadsetEx.m
@@ -1,11 +1,17 @@
+% Face Recognition using Independent Component Analysis (ICA)
+% Created on Mar 2015
+% Authors: Sid Ali Rezetane, Sina M. Baharlou, Harold Agudelo
+
 % Load the first n *.pgm images (with offset 'o') from the m specified directory to a matrix
-function D=loadsetEx(directory,n,m,o)
-D=[];
-for i=1:m
-dirEx=sprintf(directory,i);
-file_list=dir([dirEx '/*.pgm']);
-images={file_list.name};
-for i=1:n
-D=[D,loadpgm(   char(strcat(dirEx,images(i+o)))    )];
-end 
-end
+function D = loadsetEx(directory, n, m, o)
+    D = [];
+    for i = 1:m
+        dirEx = sprintf(directory, i);
+        file_list = dir([dirEx '/*.pgm']);
+        images = {file_list.name};
+        for i = 1:n
+            D = [D, loadpgm(char(strcat(dirEx, images(i + o))))];
+        end
+    end
+ 
+ 
