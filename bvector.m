002decf3a677c373530bb0d110e6f0e8d5b07c03
Sina <Sina.baharlou@gmail.com>
2019-05-22 12:41:10 +0430

    Enhanced coding style and comments.

--- bvector.m
+++ bvector.m
@@ -1,10 +1,13 @@
-% create binary vector 
-% n = size of the vector 
-% m = index of high bit 
+% Face Recognition using Independent Component Analysis (ICA)
+% Created on Mar 2015
+% Authors: Sid Ali Rezetane, Sina M. Baharlou, Harold Agudelo
+% create binary vector
+% n = size of the vector
+% m = index of high bit
 
-function v=bvector(n,m)
-% create vector 
-v=zeros(n,1);
-% put m element to high 
-v(m)=1;
+function v = bvector(n, m)
+    % create vector
+    v = zeros(n, 1);
+    % put m element to high
+    v(m) = 1;
 end
