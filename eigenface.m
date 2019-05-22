6ec548d928a10ad700f5afbbbcf32fad0b1128ed
Sina <Sina.baharlou@gmail.com>
2019-05-22 12:47:31 +0430

    Enhanced coding style and comments.

--- eigenface.m
+++ eigenface.m
@@ -1,37 +1,35 @@
-% calculate the eigenvalues 
+% Face Recognition using Independent Component Analysis (ICA)
+% Created on Mar 2015
+% Authors: Sid Ali Rezetane, Sina M. Baharlou, Harold Agudelo
+% calculate the eigenvalues
 % V is the matrix containing the eigen vectors
-% E is the matrix containing the eigen values 
-function [V,E]=eigen(M)
-
-
-% calculate the size of the matrix
-m_size=size(M);
-
-% Calculate the covariance 
-c=1/(m_size(2)-1)*(M'*M);%cov(M);
-
-% Calculate the eigen vectors of M'
-[U,E]=eig(c);
-
-
-
-
-% sort eigen vectors 
-[sorted_eig, index] = sort(sum(E));
-index=fliplr(index);
-sorted_eig=fliplr(sorted_eig);
-
-V = U(:,[index]);
-
-% calculate the eigen vectors for M
-V=M*V;
-
-% calcuate the eigen values 
-E=sqrt(sum(V.^2));
-
-% normalize the eigenvectors 
-V = V ./ (ones(m_size(1),1) * E);
-
-E=sorted_eig;
-
+% E is the matrix containing the eigen values
+function [V, E] = eigen(M)
+ 
+    % calculate the size of the matrix
+    m_size = size(M);
+ 
+    % calculate the covariance
+     c = 1 / (m_size(2) - 1) * (M'*M);%cov(M);
+ 
+    % calculate the eigen vectors of M'
+    [U, E] = eig(c);
+ 
+    % sort eigen vectors
+    [sorted_eig, index] = sort(sum(E));
+    index = fliplr(index);
+    sorted_eig = fliplr(sorted_eig);
+    V = U(:, [index]);
+ 
+    % calculate the eigen vectors for M
+    V = M * V;
+ 
+    % calcuate the eigen values
+    E = sqrt(sum(V .^ 2));
+ 
+    % normalize the eigenvectors
+    V = V ./ (ones(m_size(1), 1) * E);
+ 
+    E = sorted_eig;
+ 
 end
