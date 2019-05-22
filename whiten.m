8b4ea2864bac76279eb19dc11e8e3646d00c2753
Sina <Sina.baharlou@gmail.com>
2019-05-22 13:38:22 +0430

    Enhanced coding style and comments.

--- whiten.m
+++ whiten.m
@@ -1,20 +1,22 @@
-% whiten the data 
-% x = input matrix
-% X = output whitened data
-% WZ = whiten matrix 
-
-function [X,WZ]=whiten(x)
-
-% subtract the mean 
-[SM,M]=subMean(x);
-
-% calculate the covariance matrix of data'
-c=cov(SM');
-
-% get the whiten matrix 
-WZ=2*inv(sqrtm(c))  ;
+% Face Recognition using Independent Component Analysis (ICA)
+% Created on Mar 2015
+% Authors: Sid Ali Rezetane, Sina M. Baharlou, Harold Agudelo
 
 % whiten the data
-X=WZ*SM;
+% x = input matrix
+% X = output whitened data
+% WZ = whiten matrix
 
+function [X, WZ] = whiten(x)
+    % subtract the mean
+    [SM, M] = subMean(x);
+ 
+    % calculate the covariance matrix of data'
+    c = cov(SM');
+ 
+    % get the whiten matrix
+    WZ = 2 * inv(sqrtm(c));
+ 
+    % whiten the data
+    X = WZ * SM;
 end
