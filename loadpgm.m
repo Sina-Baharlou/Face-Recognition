07ba8655ca7ed3c8d73b8af4cbc72954c2c68529
Sina <Sina.baharlou@gmail.com>
2019-05-22 13:02:25 +0430

    Enhanced coding style and comments.

--- loadpgm.m
+++ loadpgm.m
@@ -1,16 +1,18 @@
-% simply load the pgm file to the memory convert it to grayscale and reshape it to a vector 
+% Face Recognition using Independent Component Analysis (ICA)
+% Created on Mar 2015
+% Authors: Sid Ali Rezetane, Sina M. Baharlou, Harold Agudelo
 
-function X=loadpgm(filename)
-% read the image file 
-img=imread(filename);
-
-% convert it to gray scale ( also this convert the image to double)
-gray=mat2gray(img);
-
-% get image size 
-imsize=size(gray);
-
-% reshape the image into single column vector
-X=reshape(gray,[imsize(1)*imsize(2),1]);
-
-
+% simply load the pgm file to the memory convert it to grayscale and reshape it to a vector
+function X = loadpgm(filename)
+    % read the image file
+    img = imread(filename);
+ 
+    % convert it to gray scale ( also this convert the image to double)
+    gray = mat2gray(img);
+ 
+    % get image size
+    imsize = size(gray);
+ 
+    % reshape the image into single column vector
+    X = reshape(gray, [imsize(1) * imsize(2), 1]);
+ 
