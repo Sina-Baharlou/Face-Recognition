f918922bb7755c0f6b83c244b33b4323576881f4
Sina <Sina.baharlou@gmail.com>
2019-05-22 13:37:41 +0430

    Enhanced coding style and comments.

--- subMean.m
+++ subMean.m
@@ -1,11 +1,15 @@
-%*** subtract mean
+% Face Recognition using Independent Component Analysis (ICA)
+% Created on Mar 2015
+% Authors: Sid Ali Rezetane, Sina M. Baharlou, Harold Agudelo
+
+% subtract mean
 % ZM= Zero-mean data
 % M = data mean
-  
-function [ZM,M]=subMean(D)
-% calculate the size of the matrix
-m_size=size(D);
-% calculate the mean (mean of each row)
-M=mean(D');
-ZM=D-(ones(m_size(2),1)*M)';
+
+function [ZM, M] = subMean(D)
+    % calculate the size of the matrix
+    m_size = size(D);
+    % calculate the mean (mean of each row)
+    M = mean(D');
+    ZM = D - (ones(m_size(2), 1) * M)';
 end
