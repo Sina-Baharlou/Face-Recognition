e10d89ec7c89b6148b143130fc45c4bafb161015
Sina <Sina.baharlou@gmail.com>
2019-05-22 12:51:44 +0430

    Enhanced coding style and comments.

--- getN.m
+++ getN.m
@@ -1,10 +1,14 @@
-function n=getN(x,thresh)
-total_length =sum(x);
-s=length(x);
-for i=1 : s
-	if ((sum(x(1: s-i))  / total_length)<thresh)
- 		break
-	end
-end
-n=s-i+1;
+% Face Recognition using Independent Component Analysis (ICA)
+% Created on Mar 2015
+% Authors: Sid Ali Rezetane, Sina M. Baharlou, Harold Agudelo
+
+function n = getN(x, thresh)
+    total_length = sum(x);
+    s = length(x);
+    for i = 1 : s
+        if ((sum(x(1: s - i)) / total_length) < thresh)
+            break
+        end
+    end
+    n = s - i + 1;
 end
